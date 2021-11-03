0caeb42c44f958db235bb54702cc4f181e0b1c65
A. Rizhenkov <a@rizhenkov.by>
2021-11-03 09:31:14 +0100

    Добавлена аналитика, уважающая приватность

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,30 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+  </Layout>
+</template>
+
+<script>
+
+export default {
+
+        mounted() {
+            let metricsScript = document.createElement('script');
+            metricsScript.async = true;
+            metricsScript.defer = true;
+            metricsScript.setAttribute('data-website-id', 'f69654e6-6d2f-4f4e-a9de-200410655b88')
+            metricsScript.setAttribute('data-auto-track', 'false')
+            metricsScript.setAttribute('src', 'https://umami-murex-five.vercel.app/umami.js')
+            metricsScript.onload = function(){
+                umami.trackView('/');
+            };
+            document.head.appendChild(metricsScript)
+        }
+    }
+
+
+</script>
